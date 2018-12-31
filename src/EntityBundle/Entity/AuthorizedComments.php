281c3a91289e38ff7cad095b5b828cc47e6b28c4
baycii <44557503+baycii@users.noreply.github.com>
2018-12-31 01:23:30 +0100

    Wassim Derbel Commentig System
    Commentig System

---
+++ src/EntityBundle/Entity/AuthorizedComments.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace EntityBundle\Entity;
+
+/**
+ * AuthorizedComments
+ */
+class AuthorizedComments
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $comment;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set comment
+     *
+     * @param string $comment
+     *
+     * @return AuthorizedComments
+     */
+    public function setComment($comment)
+    {
+        $this->comment = $comment;
+
+        return $this;
+    }
+
+    /**
+     * Get comment
+     *
+     * @return string
+     */
+    public function getComment()
+    {
+        return $this->comment;
+    }
+}
+
