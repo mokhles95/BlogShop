922b7cac0b00648d6e5fc36c26469ea036c47ab5
chaymaBN <39051650+chaymaBN@users.noreply.github.com>
2019-01-30 13:14:31 +0100

    chayma

---
+++ src/EntityBundle/Form/DetailCommandeType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace EntityBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class DetailCommandeType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('qte')->add('commande')->add('produit');
+    }/**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'EntityBundle\Entity\DetailCommande'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'entitybundle_detailcommande';
+    }
+
+
+}
